74ee7e7feed45e020ae0286ffba800c2ad040189
Sherman Leung <shermankleung@gmail.com>
2016-09-22 01:06:49 -0400

    in-class demo

---
+++ Assignment1/HealthFinderTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  HealthFinderTableViewCell.swift
+//  Assignment1
+//
+//  Created by Sherman Leung on 9/21/16.
+//  Copyright © 2016 Sherman Leung. All rights reserved.
+//
+
+import UIKit
+
+class HealthFinderTableViewCell: UITableViewCell {
+    @IBOutlet weak var topicImage: UIImageView!
+    @IBOutlet weak var topicTitle: UILabel!
+    @IBOutlet weak var topicLastUpdated: UILabel!
+}
--- Assignment1/ViewController.swift
+++ Assignment1/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import AFNetworking
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    var topics: [NSDictionary]?
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.rowHeight = 110.0;
+        
+        let url = URL(string: "https://healthfinder.gov/developer/MyHFSearch.json?api_key=demo_api_key&who=child&age=16&gender=male")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
+        
+        let task = session.dataTask(with: request) { (dataOrNil, response, err) in
+            if let data = dataOrNil {
+                if let responseDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                    if let results = responseDictionary["Result"] as? NSDictionary {
+                        print("response \(results["Topics"])")
+                        self.topics = results["Topics"] as? [NSDictionary]
+                        self.tableView.reloadData()
+                    }
+                }
+            }
+        }
+        task.resume()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let topics = topics {
+            return topics.count
+        }
+        return 0
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "healthFinderTableViewCell") as! HealthFinderTableViewCell
+        let topic = topics![indexPath.row]
+        let url = URL(string: topic["ImageUrl"] as! String)
+        cell.topicImage.setImageWith(url!)
+        cell.topicTitle.text = (topic["Title"] as! String).removingPercentEncoding
+        return cell
+    }
 }
 
